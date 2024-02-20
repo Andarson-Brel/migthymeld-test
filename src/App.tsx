5a9a5465c943cc653359ae6c18922f8fac3d84c9
Daniel Andara Innocent <106690294+Andarson-Brel@users.noreply.github.com>
2024-02-20 17:10:21 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Explore from './Explore'
+import Feature from './Feature'
+import Footer from './Footer'
+import Header from './Header'
+import Hero from './Hero'
+import Subscribe from './Subscribe'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Explore />
+      <Feature />
+      <Subscribe />
+      <Footer />
+    </>
+  )
+}
+
+export default App
